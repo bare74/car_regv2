e4565bb36d9a7aee002841f17ee24e9d144de2d4
bare74 <bjornarenielsen@yahoo.no>
2022-11-09 23:22:27 +0100

    asda

--- src/navigation/CarOwned.js
+++ src/navigation/CarOwned.js
@@ -3,15 +3,6 @@ import React from "react";
 
 export default function CarOwned() {
   let AllCars = JSON.parse(localStorage.getItem("APIDataCars"));
-  // const [APIData, setAPIData] = useState([]);
-  // localStorage.setItem("APIDataCars", JSON.stringify(APIData));
-
-  // useEffect(() => {
-  //   axios.get(`http://194.32.107.29/GaAPI/`).then((response) => {
-  //     setAPIData(response.data.cars);
-  //   });
-  // }, []);
-  // let Allpersons = JSON.parse(localStorage.getItem("APIDataPerson"));
 
   // const cars = AllCars;
   const carOwned = localStorage.getItem("Car Owned");
